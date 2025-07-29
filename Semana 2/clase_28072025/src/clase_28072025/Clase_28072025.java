b3a4556e8ef78aa9d1d3819a422a7871ad570cba
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2025-07-28 18:28:34 -0600

    Clase 28 de julio: repaso variables por referencia

---
+++ Semana 2/clase_28072025/src/clase_28072025/Clase_28072025.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase_28072025;
+
+/**
+ *
+ * @author claudiacortes
+ */
+public class Clase_28072025 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Programación Orientada Objetos -> POO
+        
+        Clase: Molde 
+        Intanciar
+        Objeto: Materialización
+        
+        
+        Intanciar:
+        Memoria -> Espacio ->  Casa del objeto 
+        
+        
+        Nombre, edad, altura 
+        
+        Persona genesis = new Persona("Genesis",18, 1.68);
+        Persona Marlon = new Persona("Marlon", 18,1.75);
+        Persona pedro = Marlon;
+        
+        marlon = genesis;
+        genesis.nombre = "Claudia"; genesis.setNombre("Claudia")
+        marlon.nombre = "Juan"; marlon.setNombre("Juan");
+        
+        sout(genesis.nombre); // claudia
+        sout(marlon.nombre) //   juan
+        
+        */
+        
+        /*
+        Clase- > un nuevo tipo de dato. 
+        
+        */
+        /* 
+        TIPO_DE_DATO nombre = valor; 
+        
+        TIPO_DE_DATO nombre;
+        
+        */
+        
+    }
+    
+}
---
+++ Semana 2/clase_28072025/src/clase_28072025/Persona.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_28072025;
+
+/**
+ *
+ * @author claudiacortes
+ */
+public class Persona {
+    
+    /*
+    reglas 
+    snake_case : edad_juan
+    camelCase: edadJuan
+    PascalCase: EdadJuan
+    kebab-case : edad-juan (en java no se puede)
+    */
+    
+    //1)  variables 
+    //2) constructores / getters/setters 
+    
+//    1) atributos
+    // 2) Metodos 
+        // 1) constructor 
+        // 2) mutadores: -> getters y setters 
+        // controles de acceso -> valores solo lectura / valores de solo escritura 
+    
+    
+}
