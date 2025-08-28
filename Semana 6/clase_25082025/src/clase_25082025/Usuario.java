63fe9f2293e7d93313c3228eab4cfe70d549857d
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2025-08-27 18:17:59 -0600

    clased 27 agosto: MVC combobox

---
+++ Semana 6/clase_25082025/src/clase_25082025/Usuario.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_25082025;
+
+/**
+ *
+ * @author claudiacortes
+ */
+public class Usuario {
+    
+    private String nombre;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre ;
+    }
+    
+    
+    
+}
