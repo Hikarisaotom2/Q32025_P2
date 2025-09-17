951c345392e34b33ebb66409fa7e1fbb07539907
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2025-09-16 18:31:30 -0600

    Clase 16 de septiembre: escritura archivos binarios

---
+++ Semana 9/clase_16092025/src/clase_16092025/Persona.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_16092025;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author claudiacortes
+ */
+public class Persona implements Serializable{
+        private int edad; 
+        private String nombre;
+
+    public Persona(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "edad=" + edad + ", nombre=" + nombre + '}';
+    }
+        
+        
+}
